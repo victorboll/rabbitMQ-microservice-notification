ac2919a7ad0c160bef78b5ac0c7082fd6ec58250
Daniela <danisegd@hotmail.com>
2025-04-28 00:01:15 -0300

    Adiciona arquivos ao microsserviço de usuario

--- ms-api-user/src/providers/databases/implementations/sequelize.provider.ts
+++ ms-api-user/src/providers/databases/implementations/sequelize.provider.ts
@@ -1,22 +1,21 @@
-//Documentacao
 import { Sequelize } from "sequelize-typescript";
 import { UserEntity } from "../../../models/user/user.entity";
 
 const sequelizeConnection = new Sequelize({
-  database: "mms_user",
-  dialect: senac,
-  username: senac,
-  password: senha,
-  logging: false,
-  pool: {
-    max: 3,
-    min: 1,
-  },
-  models: [UserEntity],
+    database: 'mms_user',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'Postgres2021!',
+    logging: false,
+    pool: {
+        max: 3,
+        min: 1
+    },
+    models: [UserEntity]
 });
 
 sequelizeConnection.sync().catch((e) => {
-  return;
-});
+    return;
+})
 
-export { sequelizeConnection };
+export { sequelizeConnection }
