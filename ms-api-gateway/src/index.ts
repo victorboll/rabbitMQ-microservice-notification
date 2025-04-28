80df7b5b11392f5ebb8aad4baeab35dc1f76b7c5
Daniela <danisegd@hotmail.com>
2025-04-27 22:04:18 -0300

    Adiciona rota raiz

--- ms-api-gateway/src/index.ts
+++ ms-api-gateway/src/index.ts
@@ -5,6 +5,12 @@ rabbitMQ.init().then(() => {
   app.listen(3000, () => {
     console.log('API Gateway rodando na porta 3000 ');
   });
+
+  // Adiciona a rota para a raiz
+  app.get('/', (req, res) => {
+    res.send('API Gateway está funcionando!');
+  });
+
 }).catch((err) => {
   console.error('Erro ao iniciar o RabbitMQ:', err);
 });
